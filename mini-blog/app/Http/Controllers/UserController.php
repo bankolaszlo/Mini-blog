8afaa62622a42f1eb70ea53ee67a4b20cc54784c
Bankesz <banko1212@gmail.com>
2025-09-30 22:06:56 +0200

    home

--- mini-blog/app/Http/Controllers/UserController.php
+++ mini-blog/app/Http/Controllers/UserController.php
@@ -6,10 +6,28 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-        return view('admin.dashboard');
+
+     public function home(Request $request){
+        if($request->user()->usertype=='user'){
+            return view('dashboard');
+        }else{
+            return redirect()->route('admin.dashboard');
+        }
     }
-     public function home(){
-        return view('dashboard');
+    
+    public function index(Request $request){
+        if($request->user()->usertype=='admin'){
+            return view('admin.dashboard');
+        }else{
+            return redirect()->route('dashboard');
+        }
+    }
+
+    public function post(){
+        return view('admin.post');
+    }
+    
+    public function createpost(){
+        return view('admin.createpost');
     }
 }
--- mini-blog/routes/web.php
+++ mini-blog/routes/web.php
@@ -5,14 +5,25 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', [UserController::class, 'home'])
 ->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('admin/dashboard', [UserController::class, 'index'] )
+/* Route::get('admin/dashboard', [UserController::class, 'index'] )
 ->middleware(['auth', 'admin'])->name('admin.dashboard');
+
+Route::get('admin/dashboard/post', [UserController::class, 'post'] )
+->middleware(['auth', 'admin']); 
+
+ */
+
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('admin.dashboard');
+    Route::get('/dashboard/post', [UserController::class, 'post']);
+        Route::get('/dashboard/createpost', [UserController::class, 'create post']); 
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
